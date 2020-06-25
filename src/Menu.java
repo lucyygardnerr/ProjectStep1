71a9517f29110df5bf453bedc44b08b4b249a1c5
Lucy Gardner <gardner-l@hotmail.co.uk>
2020-06-25 15:00:46 +0100

    commit 1

--- src/Menu.java
+++ src/Menu.java
@@ -19,7 +19,7 @@ public class Menu{
 
     void start(){
         // Ride chosen is Rex Rampage
-        System.out.println("Welcome to Rex Rampage at Time Travellers Theme Park!");
+        System.out.println("Welcome to Rex Rampage!");
 
         printMenu();
 
